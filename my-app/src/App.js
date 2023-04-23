aa503cba6b09c43e8b9a46ce053b6910207edfae
mdonatelli22 <DonatelliME20@gcc.edu>
2023-04-23 15:41:02 -0400

    Initial page outline with tab view

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Navbar";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <header>HELLO JMEG WORLD NEW CODE</header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div className="container">
+        <article>
+          <h1>Calendar </h1>
+          this is our app....
+        </article>
+      </div>
     </div>
   );
 }
