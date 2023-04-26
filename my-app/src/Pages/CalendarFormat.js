79c4d71579c4228987b364ae43a3ee7b465c8639
GVMartin23 <MartinGV19@gcc.edu>
2023-04-26 14:50:51 -0400

    added event that might use

--- my-app/src/Pages/CalendarFormat.js
+++ my-app/src/Pages/CalendarFormat.js
@@ -21,18 +21,44 @@ const CalendarFormat = () => {
         ]);
       }
     };
+
+    const handleAdd = ({start, end, title}) => {
+      setEvents([
+        ...events,
+        {
+          start,
+          end,
+          title,
+        },
+      ]);
+    };
+
+    const onSelectEvent = (event) => {
+      console.log(event, "Event data");
+      // const r = window.confirm("Would you like to remove this event?")
+      // if(r === true){
+        
+      //   this.setState((prevState, props) => {
+      //     const events = [...prevState.events]
+      //     const idx = events.indexOf(event)
+      //     events.splice(idx, 1);
+      //     return { events };
+      //   });
+      // }
+    }
   
     return (
       <div>
         <Calendar
           min={new Date(0,0,0,8,0,0)}
           max={new Date(0,0,0,21,0,0)}
+          onSelectEvent={onSelectEvent}
           step={15}
           localizer={localizer}
           events={events}
           startAccessor="start"
           endAccessor="end"
-          selectable={true}
+          //selectable={true}
           onSelectSlot={handleSelect}
         />
       </div>
